9a1d705d1f0482b81af701d243f6857c308b2814
Kristine Go <kristine@patterncurator.com>
2026-02-23 20:54:18 -0500

    Reorder site header layout

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -61,17 +61,17 @@ export default function SiteHeader() {
               </Link>
 
               <Link
-                href="/moodboards"
-                className="text-[13px] hover:opacity-80 underline underline-offset-4 decoration-[0.5px]"
-              >
-                Moodboards
-              </Link>
-
-              <Link
                 href="/archive"
                 className="text-[13px] hover:opacity-80 underline underline-offset-4 decoration-[0.5px]"
               >
                 Archive
+              </Link>
+
+              <Link
+                href="/moodboards"
+                className="text-[13px] hover:opacity-80 underline underline-offset-4 decoration-[0.5px]"
+              >
+                Moodboards
               </Link>
 
               <Link
