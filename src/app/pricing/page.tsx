f4435bc5f19851c1f8499aedd749982f283093ac
Kristine Go <kristine@patterncurator.com>
2026-05-13 21:44:46 -0400

    Restore email login gate on pricing

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,7 +1,12 @@
+import EmailGate from "@/components/EmailGate";
 import PricingClient from "./PricingClient";
 
 export const dynamic = "force-dynamic";
 
 export default function PricingPage() {
-  return <PricingClient />;
+  return (
+    <EmailGate source="ci">
+      <PricingClient />
+    </EmailGate>
+  );
 }
